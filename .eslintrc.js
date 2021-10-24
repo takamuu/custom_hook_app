df408b650e339221dfdb82945efd868e38cfa402
takamuu <mr.takamuu@gmail.com>
2021-10-25 07:39:08 +0900

    Fixed settings and errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,5 +26,8 @@ module.exports = {
         "@typescript-eslint"
     ],
     "rules": {
+        // note you must disable the base rule as it can report incorrect errors
+        "no-unused-vars": "off",
+        "@typescript-eslint/no-unused-vars": ["error"]
     }
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,18 @@
-export 
+import React from "react";
+import { UserCard } from "./components/UserCard";
+import "./styles.css";
+
+const user = {
+  id: 1,
+  name: "たかし",
+  email: "123@aaa.com",
+  address: "ADDRESS"
+};
+
+export default function App() {
+  return (
+    <div className="App">
+      <UserCard user={user} />
+    </div>
+  )
+}
