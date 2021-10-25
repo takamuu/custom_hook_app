3902f8b290c63c84646f06fdba245e7a0f45e730
takamuu <mr.takamuu@gmail.com>
2021-10-25 09:43:17 +0900

    Create a custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -8,32 +8,11 @@ import { User } from "./types/api/user";
 import { UserProfile } from "./types/userProfile";
 
 export default function App() {
-  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
+
 
 
   const onClickFetchUser = () => {
-    setLoading(true);
-    setError(false);
-
-    axios
-      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
-      .then((res) => {
-        const data = res.data.map((user) => ({
-          id: user.id,
-          name: `${user.name}(${user.username})`,
-          email: user.email,
-          address: `${user.address.city}${user.address.suite}${user.address.street}`,
-        }))
-        setUserProfiles(data);
-      })
-      .catch(() => {
-        setError(true);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
+    
   };
   return (
     <div className="App">
