eefc5a9b1812575ec85ec517a48f70c865cdf21f
takamuu <mr.takamuu@gmail.com>
2021-10-25 09:30:43 +0900

    Modified tsconfig, implemented data acquisition function without using custom hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,55 @@
-import React from "react";
+/* eslint-disable no-nested-ternary */
+/* eslint-disable react/button-has-type */
+import React, { useState } from "react";
+import axios from "axios";
 import { UserCard } from "./components/UserCard";
 import "./styles.css";
-
-const user = {
-  id: 1,
-  name: "たかし",
-  email: "123@aaa.com",
-  address: "ADDRESS"
-};
+import { User } from "./types/api/user";
+import { UserProfile } from "./types/userProfile";
 
 export default function App() {
+  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+
+  const onClickFetchUser = () => {
+    setLoading(true);
+    setError(false);
+
+    axios
+      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        const data = res.data.map((user) => ({
+          id: user.id,
+          name: `${user.name}(${user.username})`,
+          email: user.email,
+          address: `${user.address.city}${user.address.suite}${user.address.street}`,
+        }))
+        setUserProfiles(data);
+      })
+      .catch(() => {
+        setError(true);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
   return (
     <div className="App">
-      <UserCard user={user} />
-    </div>
-  )
-}
+      <button onClick={onClickFetchUser}>データ取得</button>
+      <br />
+      {error ? (
+        <p style={{ color: "red" }}>データの取得に失敗しました</p>
+      ) : loading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          {userProfiles.map((user) => (
+            <UserCard key={user.id} user={user} />
+          ))}
+        </>
+      )}
+    </div>  
+  );
+};
